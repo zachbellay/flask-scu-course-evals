b47d8b9045278e1fb1489410a37550be0d805aae
Zach Bellay <zachbellay@hotmail.com>
2026-09-20 16:10:40 -0700

    Allow optional whitelist file

--- app/whitelist.py
+++ app/whitelist.py
@@ -1,7 +1,13 @@
+import os
+
+
 class Whitelist:
     def __init__(self, app, whitelist_txt_abs_path):
 
         self.whitelist = set()
+
+        if not os.path.exists(whitelist_txt_abs_path):
+            return
 
         with open(whitelist_txt_abs_path) as f:
             for email in f:
