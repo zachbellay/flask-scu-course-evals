3d27711b52cfce8795d466cf896c19561ce9278f
Zach Bellay <zachbellay@hotmail.com>
2026-09-20 17:25:29 -0700

    Honor HTTPS behind reverse proxy

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,13 @@ import os
 from flask_sslify import SSLify
 from .whitelist import Whitelist
 from flask_compress import Compress
+from werkzeug.contrib.fixers import ProxyFix
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '../' , '.env')
 load_dotenv(dotenv_path)
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 app.config.from_object(os.environ['APP_CONFIG'])
 whitelist_path = os.path.join(os.path.dirname(__file__), '../' , 'WHITELIST.txt')
 whitelist = Whitelist(app, whitelist_path)
